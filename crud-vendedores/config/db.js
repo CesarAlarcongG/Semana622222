83fc29185abd7cab9fb0dfccd612d74ece1915d8
Cristopher <nuevoemail@example.com>
2025-05-02 09:47:48 -0500

    primer

---
+++ crud-vendedores/config/db.js
@@ -0,0 +1,36 @@
+const mysql = require("mysql2/promise");
+
+const pool = mysql.createPool({
+  host: "yamabiko.proxy.rlwy.net",
+  user: "root",
+  password: "rBOOvWlPhWjKRzjRKcPKGnLeDPmkQevC",
+  database: "railway",
+  port: 12620,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
+});
+
+
+// Función para probar la conexión
+async function testConnection() {
+  try {
+    const connection = await pool.getConnection();
+    console.log("Conexión exitosa a la base de datos");
+
+    // Realizar una consulta de prueba
+    const [rows] = await connection.query("SELECT 1");
+    console.log("Consulta de prueba exitosa");
+
+    connection.release();
+    return true;
+  } catch (error) {
+    console.error("Error al conectar a la base de datos:", error.message);
+    return false;
+  }
+}
+
+// Ejecutar la prueba de conexión
+testConnection();
+
+module.exports = pool;
